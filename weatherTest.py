9e3f6b2546a9e4b6056c562620f4aa92acd109c6
Andrew Fuller <47224786+afulle12@users.noreply.github.com>
2021-12-26 18:43:24 -0700

    Update weatherTest.py

--- weatherTest.py
+++ weatherTest.py
@@ -4,7 +4,7 @@ api_key = "524091aadf92df586bfe941f3a1c9ffa"
 
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-city_name = input("Enter city name : ")
+city_name = input("Enter city name   : ")
 
 complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
@@ -22,7 +22,7 @@ if x["cod"] != "404":
 
 	weather_description = z[0]["description"]
 
-	print("Temperature: " + str(math.ceil((((current_temperature - 273.15) * 9)/5) + 32)) + " F" + "\nCurrent Conditions: " + str(' '.join(elem.capitalize() for elem in weather_description.split())))
-
+	output = ("Temperature       : " + str(math.ceil((((current_temperature - 273.15) * 9)/5) + 32)) + " F" + "\nCurrent Conditions: " + str(' '.join(elem.capitalize() for elem in weather_description.split())))
+	print(output)
 else:
 	print(" City Not Found ")
