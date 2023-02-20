8c4d02a81b3caf48280adfebb4bfed54d2658b6b
Andrew Fuller <47224786+afulle12@users.noreply.github.com>
2023-02-19 19:04:17 -0700

    Update weatherBot.py

--- weatherBot.py
+++ weatherBot.py
@@ -5,20 +5,23 @@ api_key = "API Key from OpenWeatherMap"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 def weatherPrinter(city_name):
-	complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-	response = requests.get(complete_url)
-	x = response.json()
-	if x["cod"] != "404":
-		y = x["main"]
-		current_temperature = y["temp"]
-		z = x["weather"]
-		weather_description = z[0]["description"]
-		output = ("Current conditions in " + city_name + ",\nTemperature           : " + str(math.ceil((((current_temperature - 273.15) * 9)/5) + 32)) + " F" + "\nCurrent Conditions: " + str(' '.join(elem.capitalize() for elem in weather_description.split())))
-		return output
-	else:
-		return "City name not found"
+        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+        response = requests.get(complete_url)
+        x = response.json()
+        if x["cod"] != "404":
+                y = x["main"]
+                current_temperature = y["temp"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
+                output = ("Current conditions in " + city_name + ",\nTemperature           : " + str(math.ceil((((current_temperature - 273.15) * 9)/5) + 32)) + " F" + "\nCurrent Conditions: " + str(' '.join(elem.capitalize() for elem in weather_description.split())))
+                return output
+        else:
+                return "City name not found"
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_message(message):
